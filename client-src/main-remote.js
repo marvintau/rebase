5abba7bace5dec13550f358c10727f547c395f30
marvintau <nightvista@gmail.com>
2019-10-19 13:57:28 +0800

    port issue

--- client-src/main-remote.js
+++ client-src/main-remote.js
@@ -2,6 +2,6 @@ import React from 'react';
 import {render} from "react-dom";
 import App from './App.js';
 
-const address = 'http://120.77.243.97:8080';
+const address = 'http://120.77.243.97:80';
 
 render(<App address={address}/>, document.getElementById('root'));
