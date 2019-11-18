b60b0d1a6299e9b9054e746f318cb75ce84a74a6
marvintau <nightvista@gmail.com>
2019-11-18 16:00:07 +0800

    url

--- client-src/main.js
+++ client-src/main.js
@@ -4,6 +4,4 @@ import App from './App.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const address = 'http://localhost:8080';
-
-render(<App address={address}/>, document.getElementById('root'));
+render(<App address={window.location.host}/>, document.getElementById('root'));
